691d779b41f76f6101e1109d67a59e942ae8dd51
valentinagalvis <danniittasanchez@gmail.com>
2024-07-31 19:32:58 -0500

    se crea la clase SumarNumeros.java

---
+++ SumarNumeros/src/main/java/com/mycompany/sumarnumeros/SumarNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sumarnumeros;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class SumarNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
